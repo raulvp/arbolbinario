39802b1e27ea157e88edcd9f9d3128c7ba173cce
rvera <raulvp9@gmail.com>
2020-11-27 02:09:50 -0400

    recorridos del arbol

--- src/aplicacionarbolbinario/AplicacionArbolBinario.java
+++ src/aplicacionarbolbinario/AplicacionArbolBinario.java
@@ -16,24 +16,51 @@ public class AplicacionArbolBinario {
             try{
                 opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
                         "1. Agregar un nodo \n"
-                        +"2. Salir \n"
-                        +"Elegir una opcion.......","Arbol Binario"));
+                        +"2. Recorrido InOrden \n"
+                        +"3. Recorrido PreOrden \n"
+                        +"4. Recorrido PostOrden \n"
+                        +"5. Salir \n"
+                        +"Elegir una opcion.......","Arbol Binario",JOptionPane.QUESTION_MESSAGE));
                 switch(opcion){
                     case 1: elemento = Integer.parseInt(JOptionPane.showInputDialog(null,
                             "Ingrese un valor para el nodo", "Agregando Nodo",JOptionPane.QUESTION_MESSAGE));
                             
                     arbol.agregarNodo(elemento);
                     break;
-                    
-                    case 2: JOptionPane.showMessageDialog(null,"Aplicacion finalizada","Cerrando"
+                    case 2:
+                        if(!arbol.estaVacio()){
+                            arbol.inOrden(arbol.raiz);
+                        }else{
+                            JOptionPane.showMessageDialog(null,"TU arbol esta vacio","Debes llenar el mismo"
                             ,JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    case 3:
+                        if(!arbol.estaVacio()){
+                            arbol.preOrden(arbol.raiz);
+                        }else{
+                            JOptionPane.showMessageDialog(null,"TU arbol esta vacio","Debes llenar el mismo"
+                            ,JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    case 4:
+                        if(!arbol.estaVacio()){
+                            arbol.postOrden(arbol.raiz);
+                        }else{
+                            JOptionPane.showMessageDialog(null,"TU arbol esta vacio","Debes llenar el mismo"
+                            ,JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    case 5: JOptionPane.showMessageDialog(null,"Aplicacion finalizada","Cerrando"
+                            ,JOptionPane.INFORMATION_MESSAGE);
+                    break;
                     default: JOptionPane.showMessageDialog(null,"Cuidado"+JOptionPane.INFORMATION_MESSAGE);
                 }
             }catch(NumberFormatException n){
                 JOptionPane.showMessageDialog(null,"Error"+n.getMessage());
             }
         
-        }while(opcion!=2);
+        }while(opcion!=5);
         
     }
     
